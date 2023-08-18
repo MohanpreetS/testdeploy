6e02843ddf1bbd30342f11f93274a9c07f08e39c
Mohanpreet Singh Narang <mps5199mpsn.com>
2023-08-18 18:58:35 -0400

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,14 @@ function App() {
 
   return (
     <>
-      <div>
+      <h1> Hello from React</h1>
+    </>
+  )
+}
+
+export default App
+
+{/* <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -27,9 +34,4 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
+      </p> */}
